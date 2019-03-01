d03753618bb784f474f0d7ed1a643f5add0cd13c
LaurenAlmeida <39421295+LaurenAlmeida@users.noreply.github.com>
2019-03-01 12:35:32 -0300

    Add files via upload
    ok

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+  <title>Gerador de QR Code - PHP</title>
+</head>
+<body>
+
+  <h1>Gerador de QR Code </h1>
+  <form>
+<input type="hidden" value="http://www.google.com" id="texto" ?>
+    </form>
+
+<?php
+
+$var = 'code/php/qr_img.php?';
+$var .= 'd=Gerador de QR Code';
+
+
+
+ ?>
+<div style="float: left;border: 1px solid #000;">
+  <img src="<?php echo $var;?>" />
+</div>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript">
+$('#botao').click(function(e){
+e.preventDefault();
+var texto = $('#texto').val();
+
+if(texto.lenght == 0){
+  alert("Digite o texto");
+  return false;
+}
+
+//alert('code/php/qr_img.php?d='+texto);
+				$('img').attr('src','code/php/qr_img.php?d='+texto);
+})
+
+</script>
+<input type="hidden" name="id" value="$('#texto')">
+<button type="submit"
+</body>
+</html>
